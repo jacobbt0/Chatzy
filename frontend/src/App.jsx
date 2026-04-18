924a575490de0be2711d11e1849514af1ad21cff
jacob <jacobbt07@gmail.com>
2026-04-18 10:51:22 +0530

    Npm version changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
         <Route path='/' element={user ? <Home /> : <Navigate to="/login" />} />
         <Route path='/login' element={!user ? <Login /> : <Navigate to="/" />} />
         <Route path='/signup' element={!user ? <Signup /> : <Navigate to="/" />} />
-        <Route path='/profile' element={user ? <Profile /> : <Navigate to="/" />} />
+        <Route path='/profile' element={user ? <Profile /> : <Navigate to="/" />} /> 
       </Routes>
       <Toaster />
     </div>
